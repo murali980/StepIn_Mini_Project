d52c201a5677a4d7fbb70459cf80cc9c23fbb471
murali980 <muralikrishna0980@gmail.com>
2021-04-15 17:56:48 +0530

    test

--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -1,62 +1,52 @@
-#include<stdio.h>
-#include"employee.h"
-//#include "unity.h"
-void setUp()
-{
-}
-void tearDown()
-{
-}
-void add();
-void delete();
-void modify();
-void list();
-void exit();
+#include "unity.h"
+#include <employee_records.h>
+
+/* Modified */
+#include <emplyee_record.h>
+#define PROJECT_NAME    "employee"
+void add( void );
+void delete( void );
+void modify(void);
+void list(void);
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+/* Start of the application test */
 int main()
 {
-    FILE *fp, *ft; 
-    char another, choice;
-    struct emp
-    {
-        char name[40]; 
-        int DOB,age;
-		char ms[40];
-		char qualification[40];
-		char employement[40]; 
-        float bs; 
-    };
-/*  struct emp e; 
-    char empname[40]; 
-    long int recsize; 
-    fp = fopen("EMP.DAT","rb+");
-    if(fp == NULL)
-    {
-        fp = fopen("EMP.DAT","wb+");
-        if(fp == NULL)
-        {
-            printf("Connot open file");
-            exit(1);
-        }
-    }
-    recsize = sizeof(e);*/
-    switch(a)
-    {
-      case 1:
-      add( );
-      break;
-      case 2:
-      delete(); 
-      break;
-      case 3:
-      modify();
-      break;
-      case 4:
-      list();
-      break;
-      case 4:
-      exit();
-      break;
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
 
-    }    
-     
+/* Run Test functions */
+  RUN_TEST(add);
+  RUN_TEST(delete);
+  RUN_TEST(modify);
+  RUN_TEST(list);
+  
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
+}
+
+/* Write all the test functions */ 
+void add( void ) {
+  TEST_ASSERT_EQUAL('*', add( void ));
+  
+}
+
+void delete( void ) {
+  TEST_ASSERT_EQUAL('B', delete( void ));
+  
+  /* Dummy fail*/
+  // TEST_ASSERT_EQUAL(1, subtract(1000, 900));
+}
+void modify( void ) {
+  TEST_ASSERT_EQUAL('*', modify( void ));
+  
+}
+void list( void ) {
+  TEST_ASSERT_EQUAL('*', list( void ));
+  
 }
